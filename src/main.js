788243f4c438e23a1dae4bb29c53253b1b608fc0
viana s <vianasamavat@gmail.com>
2025-04-04 12:40:04 +0330

    tamrinPhone

---
+++ src/main.js
@@ -0,0 +1,304 @@
+
+let x = [
+  {
+    brand: "Apple",
+    price: "237",
+    img: "https://m.media-amazon.com/images/I/51fYXSnSu9L._AC_SY879_.jpg",
+    model: "Apple iPhone 12",
+    color: "bg-[#000000]",
+  },
+  {
+    brand: "Apple",
+    price: "699",
+    img: "./other/img/iphone/16.webp",
+    model: "Apple iPhone 16",
+    color: "bg-[#444444]",
+  },
+  {
+    brand: "Apple",
+    price: "837",
+    img: "./other/img/iphone/refurb-iphone-14-pro-max-deeppurple-202404.jpeg",
+    model: "Apple iPhone 14 pro max",
+    color: "bg-[#544960]",
+  },
+  {
+    brand: "Apple",
+    price: "1037",
+    img: "./other/img/iphone/15.jpg",
+    model: "Apple iPhone 15",
+    color: "bg-[#000000]",
+  },
+  {
+    brand: "Apple",
+    price: "1700",
+    img: "./other/img/iphone/16b.jpg",
+    model: "Apple iPhone 16B",
+    color: "bg-[#8d95cd]",
+  },
+  {
+    brand: "Samsung",
+    price: "337",
+    img: "./other/img/samsung/s20.webp",
+    model: "Galexy S20",
+    color: "bg-[#454967]",
+  },
+  {
+    brand: "Samsung",
+    price: "590",
+    img: "./other/img/samsung/s21.webp",
+    model: "Galexy S21",
+    color: "bg-[#000000]",
+  },
+  {
+    brand: "Samsung",
+    price: "705",
+    img: "./other/img/samsung/s22.webp",
+    model: "Galexy S22",
+    color: "bg-[#000000]",
+  },
+  {
+    brand: "Samsung",
+    price: "1599",
+    img: "./other/img/samsung/s25.webp",
+    model: "Galexy S25",
+    color: "bg-[#d0d0d1]",
+  },
+  {
+    brand: "Samsung",
+    price: "1400",
+    img: "./other/img/samsung/s24.webp",
+    model: "Galexy S24",
+    color: "bg-[#6e6575]",
+  },
+  {
+    brand: "Xiaomi",
+    price: "1150",
+    img: "./other/img/xiaomi/13ultra.webp",
+    model: "13 Ultra",
+    color: "bg-[#000000]",
+  },
+  {
+    brand: "Xiaomi",
+    price: "800",
+    img: "./other/img/xiaomi/pocoC65.webp",
+    model: "Poco C65",
+    color: "bg-[#c6e1f1]",
+  },
+  {
+    brand: "Xiaomi",
+    price: "1480",
+    img: "./other/img/xiaomi/pocoM6.webp",
+    model: "Poco M6",
+    color: "bg-[#000000]",
+  },
+  {
+    brand: "Xiaomi",
+    price: "1300",
+    img: "./other/img/xiaomi/redmi note 13.webp",
+    model: "Redmi Note 13",
+    color: "bg-[#000000]",
+  },
+  {
+    brand: "Xiaomi",
+    price: "700",
+    img: "./other/img/xiaomi/redmiA3.webp",
+    model: "Redmi A3",
+    color: "bg-[#93bfdb]",
+  },
+  {
+    brand: "Nothing-phone",
+    price: "600",
+    img: "./other/img/nothingPhone/1.webp",
+    model: "1",
+    color: "bg-[#4b4b4b]",
+  },
+  {
+    brand: "Nothing-phone",
+    price: "800",
+    img: "./other/img/nothingPhone/2.png",
+    model: "2",
+    color: "bg-[#4b4b4b]",
+  },
+  {
+    brand: "Nothing-phone",
+    price: "1439",
+    img: "./other/img/nothingPhone/2a.avif",
+    model: "2A",
+    color: "bg-[#ffffff]",
+  },
+  {
+    brand: "Nothing-phone",
+    price: "1300",
+    img: "./other/img/nothingPhone/3a+.jpg",
+    model: "3A+",
+    color: "bg-[#a2a2a2]",
+  },
+  {
+    brand: "Nothing-phone",
+    price: "1100",
+    img: "./other/img/nothingPhone/3a.jpg",
+    model: "3A",
+    color: "bg-[#000000]",
+  },
+  {
+    brand: "Nokia",
+    price: "1400",
+    img: "./other/img/nokia/3310.jpg",
+    model: "3310",
+    color: "bg-[#000000]",
+  },
+  {
+    brand: "Nokia",
+    price: "1000",
+    img: "./other/img/nokia/g50.avif",
+    model: "G50",
+    color: "bg-[#2a4d77]",
+  },
+  {
+    brand: "Nokia",
+    price: "644",
+    img: "./other/img/nokia/x30.avif",
+    model: "X30",
+    color: "bg-[#ffffff]",
+  },
+  {
+    brand: "Nokia",
+    price: "770",
+    img: "./other/img/nokia/xr20.avif",
+    model: "XR20",
+    color: "bg-[#2e5660]",
+  },
+  {
+    brand: "Nokia",
+    price: "1070",
+    img: "./other/img/nokia/xr21.jpg",
+    model: "XR21",
+    color: "bg-[#000000]",
+  },
+  {
+    brand: "LG",
+    price: "1050",
+    img: "./other/img/lg/doro8100+.jpg",
+    model: "Doro 8100 pluse",
+    color: "bg-[#000000]",
+  },
+  {
+    brand: "LG",
+    price: "999",
+    img: "./other/img/lg/cmf.webp",
+    model: "CMF",
+    color: "bg-[#000000]",
+  },
+  {
+    brand: "LG",
+    price: "120",
+    img: "./other/img/lg/k4.webp",
+    model: "K4",
+    color: "bg-[#000000]",
+  },
+  {
+    brand: "LG",
+    price: "200",
+    img: "./other/img/lg/g6.webp",
+    model: "G6",
+    color: "bg-[#000000]",
+  },
+  {
+    brand: "LG",
+    price: "380",
+    img: "./other/img/lg/lgk8.jpg",
+    model: "LGK8",
+    color: "bg-[#000000]",
+  },
+  {
+    brand: "Honor",
+    price: "950",
+    img: "./other/img/honor/200lite.jpg",
+    model: "200 Lite",
+    color: "bg-[#5a99a1]",
+  },
+  {
+    brand: "Honor",
+    price: "600",
+    img: "./other/img/honor/200pro.jpg",
+    model: "200 pro",
+    color: "bg-[#cbf5e7]",
+  },
+  {
+    brand: "Honor",
+    price: "1100",
+    img: "./other/img/honor/magic6lite.jpg",
+    model: "Magic 6 Lite",
+    color: "bg-[#6dae98]",
+  },
+  {
+    brand: "Honor",
+    price: "900",
+    img: "./other/img/honor/magic7pro.jpg",
+    model: "Magic 7 pro",
+    color: "bg-[#000000]",
+  },
+  {
+    brand: "Honor",
+    price: "1000",
+    img: "./other/img/honor/x6b.jpg",
+    model: "X6b",
+    color: "bg-[#000000]",
+  },
+];
+
+let PhoneLabel = async () => {
+  let br = prompt("Choose  between :   Apple , Samsung , Xiaomi , Nothing-phone , LG , Nokia , Honor ... ");
+  let pr = +prompt("please enter the price of brand (0 - 2000) : ");
+  let z = x.filter((elem) => {
+    return elem.brand == br && elem.price <= pr;
+  });
+
+  let wrapper = document.querySelector("div.wrapper");
+  wrapper.innerHTML = ""; 
+
+  z.forEach((elem) => {
+    wrapper.innerHTML += `
+            <div class="phone-card w-[370px] !pt-[15px] rounded-[13px]  bg-[#ebeaea] h-[180px] flex gap-[15px]  flex-wrap justify-center ">
+
+ 
+
+        <div class="w-[130px]  h-[150px]  rounded-[10px] overflow-hidden ">
+          <img src="${elem.img}" alt="${elem.model}" class="h-full  transition-all duration-[400ms] " />
+        </div>
+
+        <div class="w-[180px]  relative h-[150px]  !mp-[20] gap-[10px] flex flex-col !pt-[25px]">
+          <span class="model font-[f01] text-[20px]">${elem.model}</span>
+          <span class="brand font-[f02] text-[#00000069] text-[13px]">${elem.brand}</span>
+          <span class="price font-[f01] text-[#ba1010] text-[12px]">${String(elem.price)+"$"}</span>
+          <div class="color-sample ${elem.color}  w-[20px] h-[20px] absolute bottom-0 right-0 rounded-[50%]"></div>
+        </div>
+
+       
+    </div>
+
+        `;
+
+
+    
+  });
+
+
+  let img=[...document.getElementsByTagName("IMG")]
+  img.forEach((elem)=>{
+    elem.addEventListener("mouseenter",function(){
+        this.classList.add("scale")
+      })
+      elem.addEventListener("mouseleave",function(){
+        this.classList.remove("scale")
+      })
+  })
+  if(z==""){
+     wrapper.innerHTML +=`<div class="font-[f01] text-[#821818] text-[16px] w-full !mt-[100px] flex justify-center"><span>Coulden't FIND anything that matches your price or brand !!
+     </br> Please write "Brand" with capital letters. </span></div>`;
+  }
+};
+
+
+
+PhoneLabel();
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html",
+    './src/**/*.{js,ts,jsx,tsx}',
+
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
